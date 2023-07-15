fcc8b6b0be16f365c151df430bec4e955a0f1b65
Movsar Makhmutov <m.movsar@gmail.com>
2023-07-16 00:30:18 +0300

    StatsClient done

--- stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -1,5 +1,6 @@
 package ru.practicum.client;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
@@ -8,20 +9,71 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
 import ru.practicum.dto.RequestDto;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
+@Slf4j
 public class StatsClient extends BaseClient {
 
     @Autowired
     public StatsClient(@Value("${stats-server.url}") String serverUrl, RestTemplateBuilder builder) {
-        super(builder
-                .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
-                .build()
-        );
+        super(builder.uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl)).build());
     }
 
-    public ResponseEntity<Object> saveHit(String app, String uri, String ip, LocalDateTime time) {
-        return post("/hit", new RequestDto());
+    public ResponseEntity<Object> save(String app, String uri, String ip, LocalDateTime time) {
+        log.info("Saving hit for app: {}, uri: {}, ip: {}, time: {}", app, uri, ip, time);
+        return post("/hit", new RequestDto(app, uri, ip, getFormattedTime(time)));
     }
+
+    public ResponseEntity<Object> get(LocalDateTime start, LocalDateTime end, List<String> uris, Boolean unique) {
+        log.info("Getting stats for start: {}, end: {}, uris: {}, unique: {}", start, end, uris, unique);
+
+        validateTimespan(start, end);
+        String endpoint = buildStatsEndpoint(uris, unique);
+        Map<String, Object> parameters = buildParameterMap(encode(start), encode(end));
+
+        return get(endpoint, parameters);
+    }
+
+    private void validateTimespan(LocalDateTime start, LocalDateTime end) {
+        log.info("Validating timespan for start: {}, end: {}", start, end);
+
+        if (start == null || end == null || start.isAfter(end)) {
+            throw new IllegalArgumentException("Invalid timespan: start: " + start + ", end: " + end);
+        }
+    }
+
+    private String buildStatsEndpoint(List<String> uris, Boolean unique) {
+        log.info("Building stats endpoint for uris: {}, unique: {}", uris, unique);
+
+        String endpoint = "/stats?start={start}&end={end}";
+        String urisParam = Optional.ofNullable(uris)
+                .filter(u -> !u.isEmpty())
+                .map(u -> u.stream()
+                        .map(uri -> "&uris=" + uri)
+                        .collect(Collectors.joining()))
+                .orElse("");
+        String uniqueParam = Optional.ofNullable(unique).map(u -> "&unique=" + u).orElse("");
+        return endpoint + urisParam + uniqueParam;
+    }
+
+    private Map<String, Object> buildParameterMap(String start, String end) {
+        return Map.of("start", start, "end", end);
+    }
+
+    private String encode(LocalDateTime time) {
+        return URLEncoder.encode(getFormattedTime(time), StandardCharsets.UTF_8);
+    }
+
+    private String getFormattedTime(LocalDateTime start) {
+        return start.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    }
+
 }
--- stats-service/stats-dto/src/main/java/ru/practicum/dto/RequestDto.java
+++ stats-service/stats-dto/src/main/java/ru/practicum/dto/RequestDto.java
@@ -1,7 +1,23 @@
 package ru.practicum.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import javax.validation.constraints.NotBlank;
+import java.time.format.DateTimeFormatter;
+
 @Data
+@AllArgsConstructor
 public class RequestDto {
+    @NotBlank
+    private String app;
+
+    @NotBlank
+    private String uri;
+
+    @NotBlank
+    private String ip;
+
+    @NotBlank
+    private String time;
 }
