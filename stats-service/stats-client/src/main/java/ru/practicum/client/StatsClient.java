3bc1d62158407449e27be0625bba92e467c74769
Movsar Makhmutov <m.movsar@gmail.com>
2023-07-15 23:40:06 +0300

    added modules to main pom.xml

--- stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
+++ stats-service/stats-client/src/main/java/ru/practicum/client/StatsClient.java
@@ -3,8 +3,12 @@ package ru.practicum.client;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.util.DefaultUriBuilderFactory;
+import ru.practicum.dto.RequestDto;
+
+import java.time.LocalDateTime;
 
 @Service
 public class StatsClient extends BaseClient {
@@ -15,5 +19,9 @@ public class StatsClient extends BaseClient {
                 .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
                 .build()
         );
+    }
+
+    public ResponseEntity<Object> saveHit(String app, String uri, String ip, LocalDateTime time) {
+        return post("/hit", new RequestDto());
     }
 }
---
+++ stats-service/stats-dto/src/main/java/ru/practicum/dto/RequestDto.java
@@ -0,0 +1,7 @@
+package ru.practicum.dto;
+
+import lombok.Data;
+
+@Data
+public class RequestDto {
+}
