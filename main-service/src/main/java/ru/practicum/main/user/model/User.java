e3da3a63f9e3c43f46ae75f4540afdf9eec8c0f7
Movsar Makhmutov <m.movsar@gmail.com>
2023-07-21 23:58:12 +0300

    User class added

---
+++ main-service/src/main/java/ru/practicum/main/user/model/User.java
@@ -0,0 +1,20 @@
+package ru.practicum.main.user.model;
+
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "name")
+    private String name;
+}
